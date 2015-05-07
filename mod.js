48bd435747ec5f669a4a63c398dfc8e7fe26272d
TimHanson85 <Tim_hanson85@live.com>
2015-05-07 11:04:04 -0700

    mods maybe

---
+++ mod.js
@@ -0,0 +1,12 @@
+function sayHello(){
+	console.log('hello wordl');
+}
+
+function superSecret(){
+	console.log('noo you cant do that');
+}
+
+module.exports = {
+	version: '0.0.1',
+	otherName: sayHello
+};
